5045bedbe53bbe305576425a266158cec33745bc
xd-manito <66268749+xd-manito@users.noreply.github.com>
2020-10-08 20:23:45 -0400

    Update index.js

--- index.js
+++ index.js
@@ -98,7 +98,6 @@ bot.on("message", message => {
   if(message.content.startsWith("maikol")){
    if(message.author.bot) return;
    message.channel.send("tu **ldita...")
-   message.channel.send("tu no halla...")
 }
   
 });
