f3067bc6da1adfe4ecee0a3706a1066fc734f31b
xd-manito <66268749+xd-manito@users.noreply.github.com>
2020-12-01 11:59:07 -0400

    Update index.js

--- index.js
+++ index.js
@@ -14,9 +14,9 @@ bot.on("ready", () => {
 });
 
 bot.on("guildMemberAdd", guildMember => {
-  let welcomeRole = guildMember.guild.roles.cache.find(role => role.name === "UNKNOWN");
+  let welcomeRole = guildMember.guild.roles.cache.find(role => role.name === "UNKNOW");
   guildMember.roles.add(welcomeRole);
-  guildMember.guild.channels.cache.get("738529769477505106").send("Bienvenidx <@${guildMember.user.id}> esto esto es CLASE CON LOS PANAS, necesitamos comprobar tu identidad así que #por-favor-identificate en ese canal, yo soy solo un bot entonces espera a que uno de los te identifiquen. https://media.discordapp.net/attachments/716403231705792594/738532236290293810/giphy_3.gif")
+  guildMember.guild.channels.cache.get("738529769477505106").send(`Bienvenidx <@${guildMember.user.id}> esto esto es CLASE CON LOS PANAS, necesitamos comprobar tu identidad así que #por-favor-identificate en ese canal, yo soy solo un bot entonces espera a que uno de los te identifiquen. https://media.discordapp.net/attachments/716403231705792594/738532236290293810/giphy_3.gif`)
 });
 
 bot.on("guildMemberRemove", member => {
