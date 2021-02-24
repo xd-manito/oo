6aae02186b0ff4b08567bbbafd96f3698d626082
xd-manito <66268749+xd-manito@users.noreply.github.com>
2021-02-24 12:21:13 -0400

    Update index.js

--- index.js
+++ index.js
@@ -9,13 +9,21 @@ bot.user.setActivity("si", {
     url: "https://www.twitch.tv/maiatler"
   .then(console.log)
   .catch(console.error);
-});
+};
   });
 
+bot.on('guildMemberAdd', guildMember =>{
+    let welcomeRole = guildMember.guild.roles.cache.find(role => role.name === 'UNKNOWN');
+
+    guildMember.roles.add(welcomeRole);
+    guildMember.guild.channels.cache.get('YOU_CHANNEL_ID').send(`Bienvenidx <@${guildMember.user.id}> esto esto es CLASE CON LOS PANAS, necesitamos comprobar tu identidad así que #por-favor-identificate en ese canal, yo soy solo un bot entonces espera a que uno de los te identifiquen.`)
+});
+
 bot.on("guildMemberAdd", guildMember => {
-  let welcomeRole = guildMember.guild.roles.cache.get("707939297763917866");
+    let welcomeRole = guildMember.guild.roles.cache.find(role => role.name === 'UNKNOWN');
   guildMember.roles.add(welcomeRole);
-  guildMember.guild.channels.cache.get("738529769477505106").send(`Bienvenidx <@${guildMember.user.id}> esto esto es CLASE CON LOS PANAS, necesitamos comprobar tu identidad así que #por-favor-identificate en ese canal, yo soy solo un bot entonces espera a que uno de los te identifiquen. https://media.discordapp.net/attachments/716403231705792594/738532236290293810/giphy_3.gif`)
+  const attachment = new Attachment("https://media.discordapp.net/attachments/716403231705792594/738532236290293810/giphy_3.gif")
+  guildMember.guild.channels.cache.get("738529769477505106").send(`Bienvenidx <@${guildMember.user.id}> esto esto es CLASE CON LOS PANAS, necesitamos comprobar tu identidad así que #por-favor-identificate en ese canal, yo soy solo un bot entonces espera a que uno de los moderadores te identifiquen.`)
 });
 
 bot.on("message", message => {
