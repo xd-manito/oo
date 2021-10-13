7ddaeb0150653f4ef7943ad11471bc609f607dac
xd-manito <66268749+xd-manito@users.noreply.github.com>
2021-10-12 22:02:45 -0400

    Update index.js

--- index.js
+++ index.js
@@ -56,5 +56,30 @@ if(message.content === ("un simp po")){
   message.react("768483493365284884")
 }
 
+if(message.content === ("ven")){
+  if(message.author.bot) return;
+  
+  const channel = message.member.voice.channel;
+  if (!channel)
+    return message.reply(
+      "¡no te veo en ningún canal de voz! entra a uno e intenta de nuevo.")
+        .then(msg => {
+    msg.delete({ timeout: 7500 })
+  });
+
+  if (!channel.permissionsFor(message.client.user).has("CONNECT"))
+    return error("¡No tengo permisos para entrar a este canal de voz! `CONNECT`");
+
+  if (!channel.permissionsFor(message.client.user).has("SPEAK"))
+    return error("¡No tengo permisos para hablar en este canal de voz! `SPEAK`");
+
+  await channel.join()
+  .then(connection => {
+    connection.voice.setSelfDeaf(true);
+
+message.react("🤝");
+
+};
+  
 });
 bot.login(process.env.BOT_TOKEN);
